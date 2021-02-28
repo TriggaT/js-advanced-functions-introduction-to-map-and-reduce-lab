3d75b6bb78666ea90c6a619527e2271b88aef143
Trayshawn <traytwebb14@yahoo.com>
2021-02-27 18:58:45 -0600

    commit tree bd489fe10fa050f6f5e0901fdf60dac872495932

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    let newArray = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray){
+    let newArray = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i])
+    }
+    return newArray
+}
+
+function mapToDouble(sourceArray){
+    let newArray = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray){
+    let newArray = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] ** 2)
+    }
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    let a = startingPoint
+    for(let i = 0; i < sourceArray.length; i++){
+       a = sourceArray[i] + a
+    }
+    return a 
+}
+
+function reduceToAllTrue(sourceArray){
+    let a = true 
+    for (let i = 0; i < sourceArray.length; i++){
+        if (!!sourceArray[i] === false)
+        a = false  
+    }
+    return a  
+}
+
+function reduceToAnyTrue(sourceArray){
+    let a = false 
+    for (let i = 0; i < sourceArray.length; i++){
+        if (!!sourceArray[i] === true)
+        a = true 
+    }
+    return a 
+}
